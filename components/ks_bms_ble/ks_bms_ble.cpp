7015f36f5a1e95b5783f01db34ce8f8bd5eca86a
Sebastian Muszynski <s.muszynski@constructiva.de>
2025-05-06 11:55:36 +0200

    Clean up

--- components/ks_bms_ble/ks_bms_ble.cpp
+++ components/ks_bms_ble/ks_bms_ble.cpp
@@ -8,35 +8,21 @@ namespace ks_bms_ble {
 static const char *const TAG = "ks_bms_ble";
 
 static const uint16_t KS_BMS_SERVICE_UUID = 0xFF00;
-static const uint16_t KS_BMS_NOTIFY_CHARACTERISTIC_UUID = 0xFF01;   // handle 0x13
-static const uint16_t KS_BMS_NOTIFY2_CHARACTERISTIC_UUID = 0xFF03;  // handle 0x18
-static const uint16_t KS_BMS_CONTROL_CHARACTERISTIC_UUID = 0xFF02;  // handle 0x15
+static const uint16_t KS_BMS_NOTIFY_CHARACTERISTIC_UUID = 0xFF01;
+static const uint16_t KS_BMS_CONTROL_CHARACTERISTIC_UUID = 0xFF02;
 
 static const uint16_t MAX_RESPONSE_SIZE = 20;
 
-static const uint8_t KS_PKT_START = 0x55;
+static const uint8_t KS_PKT_START = 0x7B;
 static const uint8_t KS_FUNCTION_REQUEST = 0x04;
 static const uint8_t KS_FUNCTION_RESPONSE = 0x14;
-static const uint8_t KS_PKT_END = 0xAA;
+static const uint8_t KS_PKT_END = 0x7D;
 
-static const uint8_t KS_FRAME_TYPE_SOFTWARE_VERSION = 0x81;
-static const uint8_t KS_FRAME_TYPE_HARDWARE_VERSION = 0x82;
-static const uint8_t KS_FRAME_TYPE_STATUS = 0x83;
-static const uint8_t KS_FRAME_TYPE_GENERAL_INFO = 0x84;
-static const uint8_t KS_FRAME_TYPE_MOSFET_STATUS = 0x85;
-static const uint8_t KS_FRAME_TYPE_GYRO = 0x86;
-static const uint8_t KS_FRAME_TYPE_TEMPERATURES = 0x87;
-static const uint8_t KS_FRAME_TYPE_CELL_VOLTAGES_1_8 = 0x88;
-static const uint8_t KS_FRAME_TYPE_CELL_VOLTAGES_9_16 = 0x89;
-static const uint8_t KS_FRAME_TYPE_CELL_VOLTAGES_17_24 = 0x8A;
-static const uint8_t KS_FRAME_TYPE_LOCATION = 0x90;
-static const uint8_t KS_FRAME_TYPE_OWNER = 0x94;
+static const uint8_t KS_FRAME_TYPE_STATUS = 0x01;
 
-static const uint8_t KS_COMMAND_QUEUE_SIZE = 6;
+static const uint8_t KS_COMMAND_QUEUE_SIZE = 1;
 static const uint8_t KS_COMMAND_QUEUE[KS_COMMAND_QUEUE_SIZE] = {
-    KS_FRAME_TYPE_STATUS,       KS_FRAME_TYPE_GENERAL_INFO,      KS_FRAME_TYPE_MOSFET_STATUS,
-    KS_FRAME_TYPE_TEMPERATURES, KS_FRAME_TYPE_CELL_VOLTAGES_1_8, KS_FRAME_TYPE_CELL_VOLTAGES_9_16,
-    // KS_FRAME_TYPE_CELL_VOLTAGES_17_24,
+    KS_FRAME_TYPE_STATUS,
 };
 
 static const uint8_t VOLTAGE_PROTECTION_ERRORS_SIZE = 16;
@@ -163,22 +149,6 @@ void KsBmsBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gat
         ESP_LOGW(TAG, "esp_ble_gattc_register_for_notify failed, status=%d", status);
       }
 
-      //      auto *char_notify2 =
-      //          this->parent_->get_characteristic(KS_BMS_SERVICE_UUID,
-      //          KS_BMS_NOTIFY2_CHARACTERISTIC_UUID);
-      //      if (char_notify2 == nullptr) {
-      //        ESP_LOGE(TAG, "[%s] No notify service found at device, not an Ks BMS..?",
-      //                 this->parent_->address_str().c_str());
-      //        break;
-      //      }
-      //      this->char_notify2_handle_ = char_notify->handle;
-      //
-      //      auto status2 = esp_ble_gattc_register_for_notify(this->parent()->get_gattc_if(),
-      //        this->parent()->get_remote_bda(), char_notify->handle);
-      //      if (status2) {
-      //        ESP_LOGW(TAG, "esp_ble_gattc_register_for_notify failed, status=%d", status2);
-      //      }
-
       auto *char_command = this->parent_->get_characteristic(KS_BMS_SERVICE_UUID, KS_BMS_CONTROL_CHARACTERISTIC_UUID);
       if (char_command == nullptr) {
         ESP_LOGE(TAG, "[%s] No control service found at device, not an BASEN BMS..?",
@@ -191,8 +161,7 @@ void KsBmsBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gat
     case ESP_GATTC_REG_FOR_NOTIFY_EVT: {
       this->node_state = espbt::ClientState::ESTABLISHED;
 
-      this->send_command_(KS_FRAME_TYPE_SOFTWARE_VERSION);
-      this->send_command_(KS_FRAME_TYPE_HARDWARE_VERSION);
+      this->send_command_(0x01);
       break;
     }
     case ESP_GATTC_NOTIFY_EVT: {
@@ -229,257 +198,10 @@ void KsBmsBle::on_ks_bms_ble_data(const uint8_t &handle, const std::vector<uint8
   uint8_t frame_type = data[2];
 
   switch (frame_type) {
-    case KS_FRAME_TYPE_SOFTWARE_VERSION:
-      this->decode_software_version_data_(data);
-      break;
-    case KS_FRAME_TYPE_HARDWARE_VERSION:
-      this->decode_hardware_version_data_(data);
-      break;
-    case KS_FRAME_TYPE_STATUS:
-      this->decode_status_data_(data);
-      break;
-    case KS_FRAME_TYPE_GENERAL_INFO:
-      this->decode_general_info_data_(data);
-      break;
-    case KS_FRAME_TYPE_MOSFET_STATUS:
-      this->decode_mosfet_status_data_(data);
-      break;
-    case KS_FRAME_TYPE_TEMPERATURES:
-      this->decode_temperature_data_(data);
-      break;
-    case KS_FRAME_TYPE_CELL_VOLTAGES_1_8:
-    case KS_FRAME_TYPE_CELL_VOLTAGES_9_16:
-    case KS_FRAME_TYPE_CELL_VOLTAGES_17_24:
-      this->decode_cell_voltages_data_(frame_type - KS_FRAME_TYPE_CELL_VOLTAGES_1_8, data);
-      break;
-    case KS_FRAME_TYPE_LOCATION:
-    case KS_FRAME_TYPE_LOCATION + 1:
-      ESP_LOGD(TAG, "The location frame isn't supported yet");
-      break;
-    case KS_FRAME_TYPE_OWNER:
-    case KS_FRAME_TYPE_OWNER + 1:
-      ESP_LOGD(TAG, "The owner frame isn't supported yet");
-      break;
     default:
       ESP_LOGW(TAG, "Unhandled response received (frame_type 0x%02X): %s", frame_type,
                format_hex_pretty(&data.front(), data.size()).c_str());
   }
-}
-
-void KsBmsBle::decode_software_version_data_(const std::vector<uint8_t> &data) {
-  ESP_LOGI(TAG, "Software version frame received");
-  ESP_LOGD(TAG, "  %s", format_hex_pretty(&data.front(), data.size()).c_str());
-
-  // Byte Len Payload      Description                      Unit  Precision
-  //  0    1  0x55         Start of frame
-  //  1    1  0x14         Frame type (Response)
-  //  2    1  0x81         Address
-
-  //  3    16 0x30 0x2e 0x31 0x2e 0x31 0x30 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00    "0.1.10"
-  this->publish_state_(this->software_version_text_sensor_, std::string(data.begin() + 3, data.end() - 1));
-
-  //  19   1  0xaa         End of frame
-}
-
-void KsBmsBle::decode_hardware_version_data_(const std::vector<uint8_t> &data) {
-  ESP_LOGI(TAG, "Hardware version frame received");
-  ESP_LOGD(TAG, "  %s", format_hex_pretty(&data.front(), data.size()).c_str());
-
-  // Byte Len Payload      Description                      Unit  Precision
-  //  0    1  0x55         Start of frame
-  //  1    1  0x14         Frame type (Response)
-  //  2    1  0x82         Address
-
-  //  3    16 0x54 0x50 0x2d 0x4c 0x54 0x35 0x35 0x00 0x54 0x42 0x00 0x00 0x00 0x00 0x00 0x00    "TP-LT55" "TB"
-  this->publish_state_(this->device_model_text_sensor_, std::string(data.begin() + 3, data.end() - 1));
-
-  //  19   1  0xaa         End of frame
-}
-
-void KsBmsBle::decode_status_data_(const std::vector<uint8_t> &data) {
-  auto ks_get_16bit = [&](size_t i) -> uint16_t { return (uint16_t(data[i + 0]) << 8) | (uint16_t(data[i + 1]) << 0); };
-
-  ESP_LOGI(TAG, "Status frame received");
-  ESP_LOGD(TAG, "  %s", format_hex_pretty(&data.front(), data.size()).c_str());
-
-  // Byte Len Payload      Description                      Unit  Precision
-  //  0    1  0x55         Start of frame
-  //  1    1  0x14         Frame type (Response)
-  //  2    1  0x83         Address
-  //  3    2  0x00 0x3c    State of charge (60%)              %   1.0    60%
-  this->publish_state_(this->state_of_charge_sensor_, ks_get_16bit(3) * 1.0f);
-
-  //  5    2  0x14 0x72    Total voltage                      V   0.01f  52.34V
-  float total_voltage = ks_get_16bit(5) * 0.01f;
-  this->publish_state_(this->total_voltage_sensor_, total_voltage);
-
-  //  7    2  0x01 0x18    Average temperature               °C   0.1f   28.0°C
-  this->publish_state_(this->average_temperature_sensor_, ((int16_t) ks_get_16bit(7)) * 0.1f);
-
-  //  9    2  0x00 0xe6    Ambient temperature               °C   0.1f   23.0°C
-  this->publish_state_(this->ambient_temperature_sensor_, ((int16_t) ks_get_16bit(9)) * 0.1f);
-
-  //  11   2  0x00 0xf0    Mosfet temperature                °C   0.1f   24.0°C
-  this->publish_state_(this->mosfet_temperature_sensor_, ((int16_t) ks_get_16bit(11)) * 0.1f);
-
-  //  13   2  0x00 0x00    Current (signed)                  A    0.01f
-  float current = ((int16_t) ks_get_16bit(13)) * 0.01f;
-  this->publish_state_(this->current_sensor_, current);
-  float power = total_voltage * current;
-  this->publish_state_(this->power_sensor_, power);
-  this->publish_state_(this->charging_power_sensor_, std::max(0.0f, power));               // 500W vs 0W -> 500W
-  this->publish_state_(this->discharging_power_sensor_, std::abs(std::min(0.0f, power)));  // -500W vs 0W -> 500W
-
-  //  15   2  0x30 0x30    Unknown (signed)
-
-  //  17   2  0x00 0x64    State of health                    %   1.0    100%
-  this->publish_state_(this->state_of_health_sensor_, ks_get_16bit(17) * 1.0f);
-
-  //  19   1  0xaa         End of frame
-}
-
-void KsBmsBle::decode_general_info_data_(const std::vector<uint8_t> &data) {
-  auto ks_get_16bit = [&](size_t i) -> uint16_t { return (uint16_t(data[i + 0]) << 8) | (uint16_t(data[i + 1]) << 0); };
-
-  ESP_LOGI(TAG, "General info frame received");
-  ESP_LOGD(TAG, "  %s", format_hex_pretty(&data.front(), data.size()).c_str());
-
-  // Byte Len Payload      Description                      Unit  Precision
-  //  0    1  0x55         Start of frame
-  //  1    1  0x14         Frame type (Response)
-  //  2    1  0x84         Address
-  //  3    1  0x10         Cell count                             1.0    16
-  //  4    1  0x04         Temp sensors                           1.0    16
-
-  //  5    2  0x59 0xd8    Nominal capaciy                   Ah   0.01f  230.00 Ah
-  this->publish_state_(this->nominal_capacity_sensor_, ks_get_16bit(5) * 0.01f);
-
-  //  7    2  0x36 0x48    Capacity remaining                Ah   0.01f  138.96 Ah
-  this->publish_state_(this->capacity_remaining_sensor_, ks_get_16bit(7) * 0.01f);
-
-  //  9    2  0x00 0x00    Cycle count                            1.0    0
-  this->publish_state_(this->charging_cycles_sensor_, ks_get_16bit(9) * 1.0f);
-
-  //  11   2  0x00 0x00    Voltage protection bitmask
-  this->publish_state_(this->voltage_protection_bitmask_sensor_, ks_get_16bit(11) * 1.0f);
-  this->publish_state_(this->voltage_protection_text_sensor_,
-                       bitmask_to_string_(VOLTAGE_PROTECTION_ERRORS, VOLTAGE_PROTECTION_ERRORS_SIZE, ks_get_16bit(11)));
-
-  //  13   2  0x00 0x00    Current protection bitmask
-  this->publish_state_(this->current_protection_bitmask_sensor_, ks_get_16bit(13) * 1.0f);
-  this->publish_state_(this->current_protection_text_sensor_,
-                       bitmask_to_string_(CURRENT_PROTECTION_ERRORS, CURRENT_PROTECTION_ERRORS_SIZE, ks_get_16bit(13)));
-
-  //  15   2  0x00 0x00    Temperature protection bitmask
-  this->publish_state_(this->temperature_protection_bitmask_sensor_, ks_get_16bit(15) * 1.0f);
-  this->publish_state_(
-      this->temperature_protection_text_sensor_,
-      bitmask_to_string_(TEMPERATURE_PROTECTION_ERRORS, TEMPERATURE_PROTECTION_ERRORS_SIZE, ks_get_16bit(15)));
-
-  //  17   2  0x00 0x00    Error bitmask
-  this->publish_state_(this->error_bitmask_sensor_, ks_get_16bit(17) * 1.0f);
-  this->publish_state_(this->errors_text_sensor_, bitmask_to_string_(ERRORS, ERRORS_SIZE, ks_get_16bit(17)));
-
-  //  19   1  0xaa         End of frame
-}
-
-void KsBmsBle::decode_mosfet_status_data_(const std::vector<uint8_t> &data) {
-  auto ks_get_16bit = [&](size_t i) -> uint16_t { return (uint16_t(data[i + 0]) << 8) | (uint16_t(data[i + 1]) << 0); };
-
-  ESP_LOGI(TAG, "Mosfet status frame received");
-  ESP_LOGD(TAG, "  %s", format_hex_pretty(&data.front(), data.size()).c_str());
-
-  // Byte Len Payload      Description                      Unit  Precision
-  //  0    1  0x55         Start of frame
-  //  1    1  0x14         Frame type (Response)
-  //  2    1  0x85         Address
-  //  3    2  0x08 0x23    Mosfet Status Bitmask (0b100000100011)
-  this->publish_state_(this->charging_binary_sensor_, (bool) check_bit_(ks_get_16bit(3), 1));
-  this->publish_state_(this->discharging_binary_sensor_, (bool) check_bit_(ks_get_16bit(3), 2));
-  this->publish_state_(this->limiting_current_binary_sensor_,
-                       check_bit_(ks_get_16bit(3), 16) || check_bit_(ks_get_16bit(3), 32));
-
-  //  5    2  0x00 0x00    Overvoltage protection bitmask
-  //  7    2  0x00 0x00    Undervoltage protection bitmask
-  //  9    2  0x00 0x00    High Alarm Bitmask
-  //  11   2  0x00 0x00    Low Alarm Bitmask
-  //  13   2  0x00 0x00    Balancing? Bitmask
-  //  15   2  0x00 0x00    Unused
-  //  17   2  0x00 0x00    Unused
-  //  19   1  0xaa         End of frame
-}
-
-void KsBmsBle::decode_temperature_data_(const std::vector<uint8_t> &data) {
-  auto ks_get_16bit = [&](size_t i) -> uint16_t { return (uint16_t(data[i + 0]) << 8) | (uint16_t(data[i + 1]) << 0); };
-
-  ESP_LOGI(TAG, "Temperature frame received");
-  ESP_LOGD(TAG, "  %s", format_hex_pretty(&data.front(), data.size()).c_str());
-
-  // Byte Len Payload      Description                      Unit  Precision
-  //  0    1  0x55         Start of frame
-  //  1    1  0x14         Frame type (Response)
-  //  2    1  0x87         Address
-  //  3    2  0x00 0xe6    Temperature 1                    °C    0.1f  23.0°C
-  //  5    2  0x00 0xe6    Temperature 2                    °C    0.1f  23.0°C
-  //  7    2  0x00 0xe6    Temperature 3                    °C    0.1f  23.0°C
-  //  9    2  0x00 0xe6    Temperature 4                    °C    0.1f  23.0°C
-  //  11   2  0x00 0x00    Temperature 5
-  //  13   2  0x00 0x00    Temperature 6
-  //  15   2  0x00 0x00    Temperature 7
-  //  17   2  0x00 0x00    Temperature 8
-  for (uint8_t i = 0; i < 8; i++) {
-    this->publish_state_(this->temperatures_[i].temperature_sensor_, ((int16_t) ks_get_16bit((i * 2) + 3)) * 0.1f);
-  }
-
-  //  19   1  0xaa         End of frame
-}
-
-void KsBmsBle::decode_cell_voltages_data_(const uint8_t &chunk, const std::vector<uint8_t> &data) {
-  auto ks_get_16bit = [&](size_t i) -> uint16_t { return (uint16_t(data[i + 0]) << 8) | (uint16_t(data[i + 1]) << 0); };
-
-  uint8_t offset = 8 * chunk;
-
-  ESP_LOGI(TAG, "Cell voltages frame (chunk %d) received", chunk);
-  ESP_LOGD(TAG, "  %s", format_hex_pretty(&data.front(), data.size()).c_str());
-
-  // Byte Len Payload      Description                      Unit  Precision
-  //  0    1  0x55         Start of frame
-  //  1    1  0x14         Frame type (Response)
-  //  2    1  0x87         Address
-  //  3    2  0x96 0x0C    Cell voltage 1                   V     0.001f  3.265V
-  //  5    2  0x97 0x0C    Cell voltage 2                   V     0.001f  3.285V
-  //  7    2  0x98 0x0C    Cell voltage 3                   V     0.001f  3.282V
-  //  9    2  0x98 0x0C    Cell voltage 4                   V     0.001f
-  //  11   2  0x96 0x0C    Cell voltage 5                   V     0.001f
-  //  13   2  0x96 0x0C    Cell voltage 6                   V     0.001f
-  //  15   2  0x98 0x0C    Cell voltage 7                   V     0.001f
-  //  17   2  0x98 0x0C    Cell voltage 8                   V     0.001f
-  for (uint8_t i = 0; i < 8; i++) {
-    float cell_voltage = ks_get_16bit((i * 2) + 3) * 0.001f;
-    if (cell_voltage > 0 && cell_voltage < this->min_cell_voltage_) {
-      this->min_cell_voltage_ = cell_voltage;
-      this->min_voltage_cell_ = i + offset + 1;
-    }
-    if (cell_voltage > this->max_cell_voltage_) {
-      this->max_cell_voltage_ = cell_voltage;
-      this->max_voltage_cell_ = i + offset + 1;
-    }
-    this->publish_state_(this->cells_[i + offset].cell_voltage_sensor_, cell_voltage);
-  }
-
-  // Publish aggregated sensors at the last chunk. Must be improved if 3 chunks are retrieved.
-  if (chunk == 1) {
-    this->publish_state_(this->min_cell_voltage_sensor_, this->min_cell_voltage_);
-    this->publish_state_(this->max_cell_voltage_sensor_, this->max_cell_voltage_);
-    this->publish_state_(this->max_voltage_cell_sensor_, (float) this->max_voltage_cell_);
-    this->publish_state_(this->min_voltage_cell_sensor_, (float) this->min_voltage_cell_);
-    this->publish_state_(this->delta_cell_voltage_sensor_, this->max_cell_voltage_ - this->min_cell_voltage_);
-
-    this->min_cell_voltage_ = 100.0f;
-    this->max_cell_voltage_ = -100.0f;
-  }
-
-  //  19   1  0xaa         End of frame
 }
 
 void KsBmsBle::dump_config() {  // NOLINT(google-readability-function-size,readability-function-size)
@@ -579,8 +301,8 @@ bool KsBmsBle::send_command_(uint8_t function) {
   uint8_t frame[4];
 
   frame[0] = KS_PKT_START;
-  frame[1] = KS_FUNCTION_REQUEST;
-  frame[2] = function;
+  frame[1] = function;
+  frame[2] = 0x00;
   frame[3] = KS_PKT_END;
 
   ESP_LOGD(TAG, "Send command (handle 0x%02X): %s", this->char_command_handle_,
