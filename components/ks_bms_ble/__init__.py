5bf279001cc4c792187eb9bb29bfa365008773a3
Sebastian Muszynski <basti@linkt.de>
2026-03-24 22:13:48 +0100

    Add minimum ESPHome version validator (2024.12.0)

--- components/ks_bms_ble/__init__.py
+++ components/ks_bms_ble/__init__.py
@@ -21,14 +21,15 @@ KS_BMS_BLE_COMPONENT_SCHEMA = cv.Schema(
     }
 )
 
-CONFIG_SCHEMA = (
+CONFIG_SCHEMA = cv.All(
+    cv.require_esphome_version(2024, 12, 0),
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(KsBmsBle),
         }
     )
     .extend(ble_client.BLE_CLIENT_SCHEMA)
-    .extend(cv.polling_component_schema("20s"))
+    .extend(cv.polling_component_schema("20s")),
 )
 
 
